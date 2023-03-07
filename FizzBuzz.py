eacb3b291572d80bdb46e894e775d2333d053c8c
ColeD1822 <89539508+ColeD1822@users.noreply.github.com>
2023-03-07 00:43:58 -0500

    Final update to FizzBuzz program
    Added comments to the program for programmers to read and understand certain parts of the program.

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,13 +1,18 @@
+#for loop that goes through a range of numbers from 1-100
 for i in range(1,101):
+    #try block to start the loop and test for errors in the program
     try:
+        #if and else statements that find multiples of 3, 5, or both 
         if(i%3==0 and i%5==0):
             print("FizzBuzz")
         elif(i%3==0):
             print("Fizz")
         elif(i%5==0):
             print("Buzz")
+        #else in this instance will print numbers that aren't a multiple of 3, 5, or both
         else:
             print(i)
+    #exception will display an error message if there's an error in the program
     except Exception as e:
         print (e)
 
