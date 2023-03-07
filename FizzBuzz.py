b436a1112b75663110121477850ed92a3d7e874e
ColeD1822 <89539508+ColeD1822@users.noreply.github.com>
2023-03-07 00:39:31 -0500

    Updated FizzBuzz Program
    Removed math library since it was not being used in this program. Added try and except. Exception was added to display an error message if the program was to run into an error. Changed the range in the for loop to 101 after I noticed that the program was stopping at 99 and not 100.

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,11 +1,13 @@
-import math
-for i in range(1,100):
-    if(i%3==0 and i%5==0):
-        print("FizzBuzz")
-    elif(i%3==0):
-        print("Fizz")
-    elif(i%5==0):
-        print("Buzz")
-    else:
-        print(i)
+for i in range(1,101):
+    try:
+        if(i%3==0 and i%5==0):
+            print("FizzBuzz")
+        elif(i%3==0):
+            print("Fizz")
+        elif(i%5==0):
+            print("Buzz")
+        else:
+            print(i)
+    except Exception as e:
+        print (e)
 
